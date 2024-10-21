0992f98faa3af6f9dd71a1b1ec96fa63b7a5d608
abolfazl1381 <abolfazlandalib@gmail.com>
2024-10-21 22:14:23 +0330

    correct openai response formatting

--- app/gpt_services.py
+++ app/gpt_services.py
@@ -5,7 +5,7 @@ from fastapi import HTTPException
 
 def gpt_service_IaC(input:str, service:str,max_tokens:int, min_tokens:int):
 
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    
 
     prompt = f"""
             Write a robust answer about {service},
@@ -14,6 +14,7 @@ def gpt_service_IaC(input:str, service:str,max_tokens:int, min_tokens:int):
 
         """
     try:
+        client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
         chat_completion = client.chat.completions.create(
             messages=[
@@ -24,8 +25,8 @@ def gpt_service_IaC(input:str, service:str,max_tokens:int, min_tokens:int):
             ],
             model="gpt-4o-mini",
         )
-        generated_text = response.choices[0].text.strip()
-        
+        generated_text = chat_completion.choices[0].message.content
+        print(generated_text)
         return generated_text
 
     except Exception as e:

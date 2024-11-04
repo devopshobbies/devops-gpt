9d2c742c7e6b2055d22af1bc7c9b20a143b54d34
Mehdi <aceparvizi@gmail.com>
2024-11-04 21:53:17 +0100

    feat(router): add react router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -18,10 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button
-          className="bg-red-800"
-          onClick={() => setCount((count) => count + 1)}
-        >
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>

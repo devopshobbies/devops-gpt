e835cb3ae8eb4d9ca8a8f4e0bf3e8ac8fa6bec74
Mehdi <aceparvizi@gmail.com>
2024-11-14 15:43:23 +0100

    feat(validator): add form validator

---
+++ web/src/utils/formValidator.tsx
@@ -0,0 +1,33 @@
+import { RegisterOptions, FieldValues } from "react-hook-form";
+import { BasicGenFields } from "../features/constants";
+
+export const validateForm = (fieldName: BasicGenFields) => {
+  let validationRules: RegisterOptions<FieldValues, BasicGenFields> = {};
+  switch (fieldName) {
+    case BasicGenFields.MIN_TOKEN:
+      validationRules = {
+        min: {
+          value: 100,
+          message: "Min value is 100",
+        },
+        max: {
+          value: 500,
+          message: "Can not be more than 500",
+        },
+      };
+      break;
+    case BasicGenFields.MAX_TOKEN:
+      validationRules = {
+        min: {
+          value: 100,
+          message: "Can not be less than 100",
+        },
+        max: {
+          value: 500,
+          message: "Max value is 500",
+        },
+      };
+      break;
+  }
+  return validationRules;
+};

3ecad6130f50fa9ba545df807f2ca3a7ee2c01b8
Mehdi <aceparvizi@gmail.com>
2024-11-14 15:48:02 +0100

    feat(chat): create chat box and dialog bubble

---
+++ web/src/components/internal-ui/ChatBox.tsx
@@ -0,0 +1,46 @@
+import { Box } from "@chakra-ui/react";
+import ChatBubble from "./ChatBubble";
+import { Message } from "../../features/model";
+import usePost from "../../hooks/usePost";
+import { ENDPOINTS, UserType } from "../../features/constants";
+import useGptStore from "../../utils/store";
+import { useEffect } from "react";
+
+interface Props {
+  messageData: Message[];
+  endpoint: ENDPOINTS;
+  request: any;
+}
+
+const ChatBox = ({ messageData, endpoint, request }: Props) => {
+  const addMessage = useGptStore((s) => s.addMessage);
+  const { data, error, isLoading } = usePost(endpoint, request);
+
+  useEffect(() => {
+    addMessage(UserType.BOT, data?.data.output);
+  }, [request, data]);
+
+  return (
+    <Box
+      w="100%"
+      shadow="2xl"
+      h="30rem"
+      rounded="2xl"
+      className="bg-transparent border border-stone-600 bg-stone-950"
+      p="1rem"
+      overflowY="auto"
+    >
+      {messageData.map((message, index) => (
+        <ChatBubble
+          isLoading={isLoading}
+          key={index}
+          message={message.content}
+          userType={message.user}
+        />
+      ))}
+      {error && <p className="text-red-700">{error.message}</p>}
+    </Box>
+  );
+};
+
+export default ChatBox;
---
+++ web/src/components/internal-ui/ChatBubble.tsx
@@ -0,0 +1,31 @@
+import { UserType } from "../../features/constants";
+
+interface Props {
+  message: string;
+  userType: UserType;
+  isLoading: boolean;
+}
+
+const ChatBubble = ({ message, userType, isLoading }: Props) => {
+  if (!message) return;
+  return (
+    <div
+      className={`chat ${
+        userType === UserType.USER ? "chat-end" : "chat-start"
+      }`}
+    >
+      <div
+        className={`chat-bubble ${
+          userType === UserType.USER ? "bg-stone-800" : ""
+        }`}
+      >
+        {userType === UserType.USER
+          ? message
+          : userType === UserType.BOT && isLoading
+          ? "Thinkin..."
+          : message}
+      </div>
+    </div>
+  );
+};
+export default ChatBubble;

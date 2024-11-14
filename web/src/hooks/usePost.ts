5472487f9315b6ae28b2eb59c8c1914e79639273
Mehdi <aceparvizi@gmail.com>
2024-11-14 15:46:34 +0100

    feat(integration): add api integration for basic generation api

---
+++ web/src/hooks/usePost.ts
@@ -0,0 +1,14 @@
+import apiClient from "../utils/apiClient";
+
+import { useQuery } from "@tanstack/react-query";
+import { ENDPOINTS } from "../features/constants";
+
+const usePost = (endpoint: ENDPOINTS, data: any) =>
+  useQuery({
+    queryKey: [endpoint],
+    queryFn: () => apiClient.post(endpoint, data),
+    retry: 2,
+    staleTime: 60,
+  });
+
+export default usePost;

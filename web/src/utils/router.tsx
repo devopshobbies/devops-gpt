bf54f5df3b8365d583863d5859a8fdf61aad5425
Mehdi <aceparvizi@gmail.com>
2024-11-16 10:31:44 +0100

    feat(error page): add error page

--- web/src/utils/router.tsx
+++ web/src/utils/router.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layouts/Layout";
-import Landing from "../components/internal-ui/Landing";
 import BasicGen from "../features/basicGen/BasicGen";
 import { ENDPOINTS } from "../features/constants";
 import BugFix from "../features/bugFix/BugFix";
+import ErrorPage from "../components/internal-ui/ErrorPage";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <BasicGen /> },
       { path: ENDPOINTS.postFix, element: <BugFix /> },

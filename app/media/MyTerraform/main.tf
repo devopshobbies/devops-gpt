225be4f8656f0eab92422d786139cd84cdcd7780
abolfazl1381 <abolfazlandalib@gmail.com>
2024-11-18 10:49:19 +0330

    feat(argocd): add argocd to terraform

---
+++ app/media/MyTerraform/main.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2" {
+  key_pair_create      = var.key_pair_create
+  key_pair_name        = var.key_pair_name
+  security_group_create = var.security_group_create
+  security_group_name   = var.security_group_name
+  security_group_ingress_rules = var.security_group_ingress_rules
+  security_group_egress_rule   = var.security_group_egress_rule
+  instance_create      = var.instance_create
+  instance_type        = var.instance_type
+}

e5f345a7e6b1e8f8845278a6ac58d0409b5c5a7d
abolfazl1381 <abolfazlandalib@gmail.com>
2024-11-19 09:44:35 +0330

    startit

---
+++ app/media/MyTerraform/modules/docker/main.tf
@@ -0,0 +1,19 @@
+
+resource "docker_image" "image" {
+  count         = var.create_image ? 1 : 0
+  name          = var.image_name
+  force_remove  = var.image_force_remove
+
+  build {
+    context = var.image_build.context
+    tag    = var.image_build.tag
+  }
+}
+
+resource "docker_container" "container" {
+  count      = var.create_container ? 1 : 0
+  image      = var.container_image
+  name       = var.container_name
+  hostname   = var.container_hostname
+  restart    = var.container_restart
+}

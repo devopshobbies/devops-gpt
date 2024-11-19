f07f80bc12b1fd881280f842896fccd5428eb442
Mehdi <aceparvizi@gmail.com>
2024-11-19 14:13:04 +0100

    feat(change port number): edit port number base on env

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PORT?: string; // Add other VITE_ variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

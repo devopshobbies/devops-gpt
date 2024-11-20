ca739930e73e7100405a00efea008db7dcf51ecd
Mehdi <aceparvizi@gmail.com>
2024-11-20 17:50:01 +0100

    fix(download hook): update download hook to take enums

--- web/src/hooks/useDownload.ts
+++ web/src/hooks/useDownload.ts
@@ -2,8 +2,9 @@ import { useCallback, useRef } from "react";
 import useGptStore from "../utils/store";
 import apiClient from "../utils/apiClient";
 import { nameGenerator } from "../utils/nameGenerator";
+import { DownloadFolders } from "../features/constants";
 
-const useDownload = (folderName: string) => {
+const useDownload = (folderName: DownloadFolders) => {
   const { isSuccess, endpoint } = useGptStore((s) => s.generatorQuery);
   const downloadRef = useRef<HTMLAnchorElement>(null);
   const downloadFile = useCallback(async () => {

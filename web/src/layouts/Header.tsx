fabc08f5f7483f7403a127e2e2f312f529149e3a
Mehdi <aceparvizi@gmail.com>
2024-11-22 14:50:14 +0100

    style(logo): update logo style

--- web/src/layouts/Header.tsx
+++ web/src/layouts/Header.tsx
@@ -6,9 +6,9 @@ const Header = () => {
   return (
     <>
       <div className="flex h-24 w-full px-10  bg-orange-700">
-        <button onClick={() => setIsOpen(true)} className="">
+        <button onClick={() => setIsOpen(true)}>
           <Image
-            className="w-[5.5rem] h-[5.5rem] rounded-md shadow-2xl"
+            className="w-[5.5rem] h-[5.5rem] rounded-md shadow-2xl border-orange-800 border "
             src="/IMG_20240909_212657_482-removebg.png"
           />
         </button>

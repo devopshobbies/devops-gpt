d18278269fd0e5413a5668657cfc939bc3209053
Abolfazl Andalib <79583121+abolfazl8131@users.noreply.github.com>
2024-11-27 17:32:24 +0330

    feat(ansible): add nginx template generator to ansible. (#97)
    * feat(nginx_ansible): recreate nginx ansible model
    
    * fix(nginx_ansible): fix nginx validator
    
    * refactor(ansible): refactor ansible function for more flexibility

--- app/models/ansible_models.py
+++ app/models/ansible_models.py
@@ -1,6 +1,16 @@
 from typing import List, Optional
 from pydantic import BaseModel, validator, ValidationError
 
-class AnsibleInstall(BaseModel):
+
+    
+class AnsibleInstallNginx(BaseModel):
     os: str = 'ubuntu'
-    tool: str = 'nginx'
+    hosts:List[str] = ['www.example.com']
+    version:str = 'latest'
+    
+    @validator("os")
+    def validator_os(cls, value):
+        valid_oss = ['ubuntu']
+        if value not in valid_oss:
+            raise ValueError(f"your selected OS must be in {valid_oss}")
+        return value

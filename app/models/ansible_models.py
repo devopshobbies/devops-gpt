eaf713a8022047a76272396a1601ad4cd068d1ac
abolfazl1381 <abolfazlandalib@gmail.com>
2024-11-27 17:19:28 +0330

    feat(nginx_ansible): recreate nginx ansible model

--- app/models/ansible_models.py
+++ app/models/ansible_models.py
@@ -1,6 +1,15 @@
 from typing import List, Optional
 from pydantic import BaseModel, validator, ValidationError
 
-class AnsibleInstall(BaseModel):
+
+    
+class AnsibleInstallNginx(BaseModel):
     os: str = 'ubuntu'
-    tool: str = 'nginx'
+    hosts:List[str] = ['www.example.com']
+    version:str = 'latest'
+    
+    @validator("os")
+    def validator_os(cls, value):
+        if value not in ['ubuntu']:
+            raise ValueError("Size must be a valid string ending with 'Gi', 'Mi', or 'Ti'.")
+        return value

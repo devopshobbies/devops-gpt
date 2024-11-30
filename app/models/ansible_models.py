ecddfa6797f868be64348574121200e915d2e05a
abolfazl1381 <abolfazlandalib@gmail.com>
2024-11-30 14:00:19 +0330

    fix(ansible kuber): add version to model

--- app/models/ansible_models.py
+++ app/models/ansible_models.py
@@ -38,6 +38,7 @@ class AnsibleInstallKuber(AnsibleBase):
     k8s_worker_nodes: List[str]
     k8s_master_nodes: List[str]
     lb_nodes: List[str]
+    version:str = 1.31.2
     
     
     @validator("os")

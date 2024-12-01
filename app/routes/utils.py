4aa2e1a20ecb135377b3650e6b40f79e63fe9e55
Abolfazl Andalib <79583121+abolfazl8131@users.noreply.github.com>
2024-12-01 10:42:11 +0330

    fix kuber version (#114)
    * update
    
    * fix(kuber ansible): fix kuber version

--- app/routes/utils.py
+++ app/routes/utils.py
@@ -14,6 +14,7 @@ def zip_folder(folder_path: str, output_zip_path: str):
                 # Add file to the zip file
                 zip_file.write(file_path, os.path.relpath(file_path, folder_path))
 
+
 def add_files_to_folder(files:list,folder:str):
     
     os.makedirs(folder, exist_ok=True)

c61adb30fe14c1f5ef5dfbdc64dbe8453026f446
abolfazl1381 <abolfazlandalib@gmail.com>
2024-12-04 23:18:50 +0330

    fix(compose): directory builder

--- app/template_generators/docker/compose.py
+++ app/template_generators/docker/compose.py
@@ -1,10 +1,18 @@
 import yaml
 from app.models.compose_models import DockerCompose
+import os
 
 def docker_compose_generator(input):
-    
+    dir = 'app/media/MyCompose'
     compose_total = input.model_dump(mode="json")
-    
+    if not os.path.exists(dir): 
+        os.makedirs(dir)
+        os.path.join(dir, 'docker-compose.yaml') 
+            
+        file=open("app/media/MyCompose/docker-compose.yaml","w")
+        yaml.dump(compose_total,file)
+        file.close()
+        
     file=open("app/media/MyCompose/docker-compose.yaml","w")
     yaml.dump(compose_total,file)
     file.close()

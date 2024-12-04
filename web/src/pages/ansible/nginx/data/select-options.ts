3b9d06ead8428a219fc30acb4608c6b5162472b6
Milad Sadeghi <miladsadeghi2323@gmail.com>
2024-12-04 21:26:37 +0330

    feat(web): Add new Nginx version options in Ansible Template service

--- web/src/pages/ansible/nginx/data/select-options.ts
+++ web/src/pages/ansible/nginx/data/select-options.ts
@@ -10,4 +10,20 @@ export const VersionOptions = [
     value: 'latest',
     label: 'Latest',
   },
+  {
+    value: '1.27.3-1',
+    label: '1.27.3-1',
+  },
+  {
+    value: '1.25.5-1',
+    label: '1.25.5-1',
+  },
+  {
+    value: '1.23.4-1',
+    label: '1.23.4-1',
+  },
+  {
+    value: '1.21.6-1',
+    label: '1.21.6-1',
+  },
 ];
